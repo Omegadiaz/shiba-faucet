8af23ef83e7ab74b15304c6e602b30e1b34631b7
ubinatus <ja.castrocabrera@gmail.com>
2021-06-05 16:44:44 -0500

    readme + requestToken comments

--- pages/api/faucet/requestTokens.js
+++ pages/api/faucet/requestTokens.js
@@ -5,6 +5,7 @@ import { connectToDatabase } from '../../../utils/mongodb';
 import { ObjectId } from 'mongodb';
 import contract from '../../../utils/contract';
 import rateLimit from '../../../utils/rateLimit';
+import { getSession } from 'next-auth/client'
 
 // SHIB contract
 const { abi: CONTRACT_ABI, address: CONTRACT_ADDRESS } = contract;
@@ -16,14 +17,36 @@ const web3js = new web3(new web3.providers.HttpProvider("https://kovan.infura.io
 const FROM_ADDRESS = process.env.MM_PRIVATE_ADDRESS;
 const FROM_KEY = Buffer.from(process.env.MM_PRIVATE_KEY, 'hex');
 
+
+// Requests to /api/faucet/requestTokens
 export default async function handler(req, res) {
   try {
+    // Express Rate Limit middleware to avoid many petitions from same IP
+    await rateLimit(req, res);
+
+    // Require each request to have a session;
+    const session = await getSession({ req });
+    if (!session) {
+      return res.status(401).json({ message: "Please, authenticate" });
+    }
+
     const { body: { address }, method } = req;
     if (method === "POST") {
-      await rateLimit(req, res);
 
+      // Validate if address is sent
+      if (!address) {
+        return res.status(401).json({ message: "Address required" });
+      }
+
+      // Validate if address sent is valid
+      if (!web3.utils.isAddress(address)) {
+        return res.status(401).json({ message: "Invalid address" });
+      }
+
+      // Connect to MongoDB database
       const { db } = await connectToDatabase()
 
+      // Get the latest fund of this addres
       const funds = await db
         .collection("funds")
         .find({ address })
@@ -32,6 +55,7 @@ export default async function handler(req, res) {
         .toArray();
 
 
+      // Check if the user has already been given funds in the last 24 hours
       if (funds.length > 0) {
         const currentDate = new Date();
         const oneDay = 60 * 60 * 24;
@@ -46,45 +70,48 @@ export default async function handler(req, res) {
       await db.collection("funds").insertOne({ address, transactionHash: 'blabla' });
       return res.status(200).json({ funds, address, transactionHash: 'bkabka' });
 
-      console.log('requestTokens.js | requesting to address', address);
+      // Declaring the Contract
       const contract = new web3js.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
 
-      let count;
 
-      web3js.eth.getTransactionCount(FROM_ADDRESS).then(function (v) {
-        console.log("Count: " + v);
-        count = v;
-        const amount = web3js.utils.toWei("1000000", "ether");
+      web3js.eth.getTransactionCount(FROM_ADDRESS)
+        .then(function (v) {
+          console.log("Count: " + v);
+          const count = v;
 
-        const txParams = {
-          nonce: web3js.utils.toHex(count),
-          gasPrice: web3js.utils.toHex(20 * 1e9),
-          gasLimit: web3js.utils.toHex(210000),
-          to: CONTRACT_ADDRESS,
-          value: '0x00',
-          data: contract.methods.transfer(address, amount).encodeABI(),
-        }
+          // Transfer 100m SHIB
+          const amount = web3js.utils.toWei("1000000", "ether");
 
-        const common = new Common({ chain: 'kovan' })
-        const tx = Transaction.fromTxData(txParams, { common })
+          // Declare Kovan Network
+          const common = new Common({ chain: 'kovan' })
 
+          // Declare transaction before signin
+          const tx = Transaction.fromTxData({
+            nonce: web3js.utils.toHex(count),
+            gasPrice: web3js.utils.toHex(20 * 1e9),
+            gasLimit: web3js.utils.toHex(210000),
+            to: CONTRACT_ADDRESS,
+            value: '0x00',
+            data: contract.methods.transfer(address, amount).encodeABI(),
+          }, { common })
 
-        const signedTx = tx.sign(FROM_KEY);
+          // Sign transaction
+          const signedTx = tx.sign(FROM_KEY);
 
-        const serializedTx = signedTx.serialize();
+          // Send signed transaction to Blockchain
+          web3js.eth.sendSignedTransaction('0x' + signedTx.serialize().toString('hex'))
+            .on('transactionHash', function (transactionHash) {
+              db.funds.insert({ address, transactionHash });
+              res.status(200).json({ address, transactionHash });
+            });
 
-        web3js.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
-          .on('transactionHash', function (transactionHash) {
-            db.funds.insert({ address, transactionHash });
-            res.status(200).json({ address, transactionHash });
-          });
+          // Get faucet balance
+          contract.methods.balanceOf(FROM_ADDRESS).call()
+            .then(function (balance) {
+              console.log('requestTokens.js | New balance', balance);
+            });
 
-        contract.methods.balanceOf(FROM_ADDRESS).call()
-          .then(function (balance) {
-            console.log('requestTokens.js | New balance', balance);
-          });
-
-      });
+        });
     } else {
       res.setHeader('Allow', ['POST'])
       res.status(405).end(`Method ${method} Not Allowed`)
