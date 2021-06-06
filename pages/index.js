c1289685042a70a72afa030033bf7874e36bfb0e
ubinatus <ja.castrocabrera@gmail.com>
2021-06-06 16:25:40 -0500

    add multiple networks: ropsten, rinkeby and kovan

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,15 @@ import { signIn, signOut, useSession } from 'next-auth/client'
 import ReCAPTCHA from "react-google-recaptcha";
 import web3Utils from 'web3-utils';
 
+const tokenAddress = {
+  ropsten: "0x34e12283f70b332a14e91a00a99396d41488b23f",
+  kovan: "0x32210fab2af4e19290b5ebc828faa9d6184d7156",
+  rinkeby: "0x34e12283f70b332a14e91a00a99396d41488b23f",
+}
+
 export default function Home() {
   const [session] = useSession();
+  const [network, setNetwork] = useState('ropsten');
   const [address, setAddress] = useState('');
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
@@ -15,22 +22,25 @@ export default function Home() {
   const recaptchaRef = createRef();
 
   const handleAddress = (e) => {
-    if(loading) return;
+    if (loading) return;
     setAddress(e.target.value);
     setError('');
     setTransactionHash('');
   }
 
   const handleSubmit = () => {
-    if(loading) return;
-    if(!session){
+    if (loading) return;
+    if (!session) {
       return setError('Please, log in to request SHIB');
     }
-    if(!address){
-      return setError('Please, enter your Kovan testnet address');
+    if (!tokenAddress[network]) {
+      return setError('Please, select a testnet network');
+    }
+    if (!address) {
+      return setError('Please, enter your testnet address');
     }
     const validAddress = web3Utils.isAddress(address);
-    if(!validAddress){
+    if (!validAddress) {
       return setError('Please, enter a valid address');
     }
 
@@ -40,46 +50,46 @@ export default function Home() {
 
     recaptchaRef.current.execute();
   };
-  
+
   const onReCAPTCHAChange = async (captchaCode) => {
     // If the reCAPTCHA code is null or undefined indicating that
     // the reCAPTCHA was expired then return early
-    if(!captchaCode) {
+    if (!captchaCode) {
       setLoading(false);
       return;
     }
     recaptchaRef.current.reset();
-    
+
     try {
       const res = await fetch('/api/faucet/requestTokens', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ address }),
+        body: JSON.stringify({ address, network }),
       });
-     
-      if(!res.ok){
+
+      if (!res.ok) {
         const errorResData = await res.json();
         throw new Error(errorResData.message);
       }
-  
+
       const data = await res.json();
-      if(data && data.transactionHash){
+      if (data && data.transactionHash) {
         setTransactionHash(data.transactionHash);
       }
-      
+
     } catch (error) {
       setError(error.message);
-    } 
+    }
     setLoading(false);
   }
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>ShibaToken Kovan Faucet</title>
-        <meta name="description" content="Shiba Token | Kovan Faucet" />
+        <title>ShibaToken Faucet</title>
+        <meta name="description" content="Shiba Token Faucet | This faucet allows you to request SHIB to be used on the Ropsten, Kovan or Rinkeby test network." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -108,7 +118,7 @@ export default function Home() {
           <div className={styles.auth}>
             <div style={{ width: 9, height: 9, borderRadius: "50%", backgroundColor: "orange" }} />
             <div>Please, authenticate</div>
-            <div onClick={() => signIn()} className={styles.authButton}>Log in</div>
+            <div onClick={() => signIn('github')} className={styles.authButton}>Log in</div>
           </div>
         )}
       </div>
@@ -126,27 +136,32 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          This faucet allows you to request Shiba Token (SHIB) to be used on the Kovan Ethereum test network.
+          This faucet allows you to request Shiba Token (SHIB) to be used on the Ethereum test networks.
         </p>
         <div className={styles.inputContainer}>
-          <input 
-            type="text" 
-            className={styles.input} 
+          <input
+            type="text"
             placeholder="Your testnet address"
             value={address}
             onChange={handleAddress}
           />
-          <div className={styles.message}>
-            {error ? (
-              <div className={styles.error}>
-                {error}
-              </div>
-            ) : transactionHash ? (
-               <div className={styles.success}>
-                 Your request was successful! View your transaction <a href={`https://kovan.etherscan.io/tx/${transactionHash}`} target="_blank" rel="noopener noreferrer">here</a>.
-               </div> 
-            ) : null}
-          </div>
+          <div className={styles.network + " " + styles[network]}/>
+          <select value={network} onChange={e => setNetwork(e.target.value)}>
+            <option className={styles.ropsten} value="ropsten">Ropsten</option>
+            <option className={styles.kovan} value="kovan">Kovan</option>
+            <option className={styles.rinkeby} value="rinkeby">Rinkeby</option>
+          </select>
+        </div>
+        <div className={styles.message}>
+          {error ? (
+            <div className={styles.error}>
+              {error}
+            </div>
+          ) : transactionHash ? (
+            <div className={styles.success}>
+              Your request was successful! View your transaction <a href={`https://${network}.etherscan.io/tx/${transactionHash}`} target="_blank" rel="noopener noreferrer">here</a>.
+            </div>
+          ) : null}
         </div>
         <button className={styles.button} onClick={handleSubmit}>
           {loading ? (
@@ -169,11 +184,19 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
+          href={`https://${network}.etherscan.io/token/${tokenAddress[network]}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Token address {tokenAddress[network]}
+        </a>
+        <div>•</div>
+        <a
           href="https://www.openshiba.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Open Shiba Foundation
+          Open Shiba
         </a>
       </footer>
     </div>
