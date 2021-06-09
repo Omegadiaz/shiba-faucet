c6ca7293180bcf9239d6f3ba77d2650e7dca3e0e
ubinatus <ja.castrocabrera@gmail.com>
2021-06-09 17:20:34 -0500

    config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,36 @@
 module.exports = {
-  target: "serverless",
-  webpack: (config, { isServer, webpack }) => {
-    if (!isServer) {
-      config.plugins.push(new webpack.IgnorePlugin(/^(mongoose|mongodb|mongodb\-client\-encryption|swarm\-js|electron)$/));
-    }
-    return config;
-  },
+  // webpack: (config, { isServer, webpack }) => {
+  //   console.log('IS SERVER', isServer)
+  //   if (!isServer) {
+  //     config.node = {
+  //       dgram: 'empty',
+  //       fs: 'empty',
+  //       net: 'empty',
+  //       tls: 'empty',
+  //       child_process: 'empty',
+  //     };
+  //     config.plugins.push(new webpack.IgnorePlugin(/^(mongoose)$/));
+  //     config.plugins.push(new webpack.IgnorePlugin(/^(mongodb)$/));
+  //     config.plugins.push(new webpack.IgnorePlugin(/^(mongodb\-client\-encryption)$/));
+  //     config.plugins.push(new webpack.IgnorePlugin(/^(swarm\-js)$/));
+  //     config.plugins.push(new webpack.IgnorePlugin(/^(electron)$/));
+  //     config.plugins.push(new webpack.IgnorePlugin({
+  //       resourceRegExp: /^\.\/lib$/,
+  //       contextRegExp: /mongodb$/,
+  //     }));
+  //     config.plugins.push(
+  //       new webpack.IgnorePlugin({
+  //         checkResource(resource, context) {
+  //           // If I am including something from my backend directory, I am sure that this shouldn't be included in my frontend bundle
+  //           if (resource.includes('/backend/') && !context.includes('node_modules')) {
+  //             return true;
+  //           }
+  //           return false;
+  //         },
+  //       }),
+  //     );
+  //   }
+  //   return config;
+  // },
   distDir: 'out',
 };
