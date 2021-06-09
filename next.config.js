7ad2d12473eda975b88b61b32d6ca91765a8b2f2
ubinatus <ja.castrocabrera@gmail.com>
2021-06-09 17:10:24 -0500

    config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   target: "serverless",
   webpack: (config, { isServer, webpack }) => {
     if (!isServer) {
-      config.plugins.push(new webpack.IgnorePlugin(/^(mongoose|mongodb|mongodb\-client\-encryption|swarm\-js)$/));
+      config.plugins.push(new webpack.IgnorePlugin(/^(mongoose|mongodb|mongodb\-client\-encryption|swarm\-js|electron)$/));
     }
     return config;
   },
