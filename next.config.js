a6b56e9128e867469dcd6d226eddf8d93a5573b6
ubinatus <ja.castrocabrera@gmail.com>
2021-06-09 17:00:17 -0500

    next config

--- next.config.js
+++ next.config.js
@@ -2,15 +2,7 @@ module.exports = {
   target: "serverless",
   webpack: (config, { isServer, webpack }) => {
     if (!isServer) {
-      config.node = {
-        dgram: 'empty',
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty',
-        child_process: 'empty',
-      };
-
-      config.plugins.push(new webpack.IgnorePlugin(/^(mongodb-client-encryption|electron)$/));
+      config.plugins.push(new webpack.IgnorePlugin(/^(mongoose|mongodb|mongodb-client-encryption|electron)$/));
     }
     return config;
   },
